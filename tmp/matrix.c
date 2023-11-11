29d09419de1a3a315a8153232f8cdabed1b2582f
Yash <yashchaurasia630@gmail.com>
2023-11-11 19:23:50 +0530

    first commit

---
+++ tmp/matrix.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]) {
+	if(argc == 2) {
+		printf("knock, Knock, %s\n", argv[1]);
+	}
+	else {
+		fprintf(stderr, "Usage: %s <name>\n", argv[0]);
+		return 1;
+	}
+	return 0;
+}
