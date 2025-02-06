739af3336304a5c3d6a49b8a6418622ed207324b
YASH <yashchaurasia630@gmail.com>
2025-02-06 21:50:19 +0530

    license_2

--- liveoverflow_youtube/0x07_0x08_uncrackable_crackme/license_2.c
+++ liveoverflow_youtube/0x07_0x08_uncrackable_crackme/license_2.c
@@ -1,20 +1,21 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-        if(argc==2) {
-		printf("Checking License: %s\n", argv[1]);
-                int sum = 0;
-                for (int i = 0; i < strlen(argv[1]); i++) {
-			sum+= (int)argv[1][i];	
-		}
-		if(sum==916) {
-			printf("Access Granted!\n");
-		} else {
-			printf("WRONG!\n");
-		}
-	} else {
-		printf("Usage: <key>\n");
-	}
-	return 0;
+  if(argc==2) {
+    int sum = 0;
+    printf("Checking Lisence: %s\n", argv[1]);
+    for(int i=0; i<strlen(argv[1]); i++) {
+      sum += (int)argv[1][i];
+    }
+    
+    if(sum == 916)
+      printf("Access Granted!\n");
+    else 
+      printf("Access Denied!\n");
+  }
+  else {
+    printf("Usage: %s <key>\n", argv[0]);
+  }
+  return 0;
 }
