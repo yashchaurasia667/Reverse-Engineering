3d2cd13ac105eb41ee28240a7898a0d8b232b0a0
YASH <yashchaurasia630@gmail.com>
2025-04-15 23:50:28 +0530

    caf; final stack exploitation challenge

---
+++ liveoverflow_youtube/0x13.5/setup.sh
@@ -0,0 +1,6 @@
+# compile the caf program
+# gcc caf.c -o caf -fno-stack-protector -z execstack -no-pie
+
+# launch server to connect to 
+# connect with: nc 127.0.0.1 1337
+sudo socat TCP-LISTEN:1337,nodelay,reuseaddr,fork EXEC:"stdbuf -i0 -o0 -e0 ./caf"
