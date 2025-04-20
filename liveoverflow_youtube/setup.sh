9f9e2ee9dafcf035d02ab335c580395d37ec5ada
YASH <yashchaurasia630@gmail.com>
2025-04-20 19:59:52 +0530

    setup script

--- liveoverflow_youtube/setup.sh
+++ liveoverflow_youtube/setup.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-# enable core dumps
-echo "enabling core dumps..."
-echo "/tmp/core.%e.%p" | sudo tee /proc/sys/kernel/core_pattern
-ulimit -S -c unlimited
+{
+echo "# enable core dumps"
+echo "echo '/tmp/core.%e.%p' | sudo tee /proc/sys/kernel/core_pattern"
+echo "ulimit -S -c unlimited"
 
-# disable aslr
-echo "disabling ASLR..."
-echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+echo "# disable aslr"
+echo "echo 0 | sudo tee /proc/sys/kernel/randomize_va_space"
+} | sudo tee /opt/reve.sh > /dev/null
 
+sudo chmod +x /opt/reve.sh
+
+{
+echo "[Unit]"
+echo "Description=Reverse Engineering setup"
+echo "[Service]"
+echo "ExecStart=/opt/reve.sh"
+echo "Restart=always"
+echo "[Install]"
+echo "WantedBy=multi-user.target"
+} | sudo tee /etc/systemd/system/revEng.service > /dev/null
+
+sudo systemctl enable revEng.service
+sudo systemctl start revEng.service
